4dccdaf497ba83631ef98fccc067e1e19ce0d544
xiaodaoshou <xiaodaoshou>
2023-07-16 22:25:02 +0800

    project1

--- code/project1/server.cpp
+++ code/project1/server.cpp
@@ -1,87 +1,67 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
-const int PORT = 8080;
-const int BUFFER_SIZE = 1024;
 
-std::string readFile(const std::string& filename) {
-    std::ifstream file(filename);
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
-}
-
-std::string generateResponse(const std::string& content) {
-    std::stringstream response;
-    response << "HTTP/1.1 200 OK\r\n";
-    response << "Content-Type: text/html\r\n";
-    response << "Content-Length: " << content.length() << "\r\n";
-    response << "\r\n";
-    response << content;
-    return response.str();
-}
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1) {
-        std::cerr << "Failed to create socket." << std::endl;
-        return 1;
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // 创建 socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "Socket creation failed" << std::endl;
+        return -1;
     }
 
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(PORT);
+    // 绑定 IP 和端口
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        std::cerr << "Failed to bind socket." << std::endl;
-        return 1;
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        return -1;
     }
 
-    if (listen(serverSocket, 5) == -1) {
-        std::cerr << "Failed to listen on socket." << std::endl;
-        return 1;
+    // 监听客户端连接
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        return -1;
     }
 
-    std::cout << "Server started. Listening on port " << PORT << "..." << std::endl;
+    std::cout << "Server started listening on port " << PORT << std::endl;
 
     while (true) {
-        sockaddr_in clientAddress{};
-        socklen_t clientAddressLength = sizeof(clientAddress);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddressLength);
-        if (clientSocket == -1) {
-            std::cerr << "Failed to accept client connection." << std::endl;
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        // 等待客户端连接
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
+            std::cerr << "Accept failed" << std::endl;
             continue;
         }
 
-        char buffer[BUFFER_SIZE];
-        ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
-        if (bytesRead == -1) {
-            std::cerr << "Failed to read from client socket." << std::endl;
-            close(clientSocket);
-            continue;
-        }
+        std::cout << "Client connected: " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl;
 
-        buffer[bytesRead] = '\0';
-        std::string request(buffer);
+        // 读取 HTTP 请求
+        memset(buffer, 0, BUFFER_SIZE);
+        read(client_fd, buffer, BUFFER_SIZE);
 
-        if (request.find("GET / HTTP") != std::string::npos) {
-            std::string content = readFile("index.html");
-            std::string response = generateResponse(content);
-            ssize_t bytesSent = send(clientSocket, response.c_str(), response.length(), 0);
-            if (bytesSent == -1) {
-                std::cerr << "Failed to send response to client." << std::endl;
-            }
-        }
+        std::cout << "Received request: " << std::endl << buffer << std::endl;
 
-        close(clientSocket);
+        // 构造 HTTP 响应
+        std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, World!</h1></body></html>";
+        send(client_fd, response.c_str(), response.length(), 0);
+
+        // 关闭客户端连接
+        close(client_fd);
     }
 
-    close(serverSocket);
     return 0;
 }
-
